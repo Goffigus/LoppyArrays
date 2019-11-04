0fd55367ff6c65449f1d1a75219b16ccedfa8185
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-11-04 09:42:55 -0500

    Add project files.

---
+++ LoppyArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace LoppyArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //<type>[<declares array] = new <same type> [<size>]
+            string[] input = new string[10];
+
+            string[] otherInput = { "rock", "rock", "rock" }; //another way to create an array
+
+            int counter = 0;
+            while(counter < 3)
+            {
+                input[counter] = ReadLine();
+                Clear();
+                counter++;
+            }
+
+            
+            while(counter > 0)
+            {
+                WriteLine("Player{0} chose {1}", counter, input[counter - 1]);
+                counter--;
+            }
+        }
+    }
+}
